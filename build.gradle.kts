a8704739279df55a98f4c2fb5c902ea70a784592
Imran Malic Settuba <46971368+i-walker@users.noreply.github.com>
2022-03-14 15:41:08 +0100

    update Js template (#6)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,46 @@
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.6.10"
+  kotlin("js") version "1.6.10"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
-    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+  mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("stdlib-js"))
-    implementation("io.arrow-kt:arrow-core:1.0.1")
-    implementation("io.arrow-kt:arrow-optics:1.0.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("io.arrow-kt:arrow-fx-coroutines:1.0.1")
-    testImplementation(kotlin("test-common"))
-    testImplementation(kotlin("test-annotations-common"))
+  implementation(kotlin("stdlib-js"))
+  implementation("io.arrow-kt:arrow-core:1.0.1")
+  implementation("io.arrow-kt:arrow-optics:1.0.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+  implementation("io.arrow-kt:arrow-fx-coroutines:1.0.1")
+
+  testImplementation("io.kotest:kotest-property:5.1.0")
+  testImplementation("io.kotest:kotest-assertions-core:5.1.0")
+  testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.2.3")
+  testImplementation("io.kotest.extensions:kotest-property-arrow:1.2.3") // optional
+  testImplementation("io.kotest.extensions:kotest-property-arrow-optics:1.2.3") // optional
 }
 
 kotlin {
-    js {
-        browser {
-            webpackTask {
-                cssSupport.enabled = true
-            }
+  js(IR) {
+    browser {
+        webpackTask {
+            cssSupport.enabled = true
+        }
 
-            runTask {
-                cssSupport.enabled = true
-            }
+        runTask {
+            cssSupport.enabled = true
+        }
 
-            testTask {
-                useKarma {
-                    useChromeHeadless()
-                    webpackConfig.cssSupport.enabled = true
-                }
+        testTask {
+            useKarma {
+                useChromeHeadless()
+                webpackConfig.cssSupport.enabled = true
             }
         }
-        binaries.executable()
     }
+    binaries.executable()
+  }
 }
